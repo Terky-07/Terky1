270c0f46567348a7c36bcc9c2e689df70b848c30
Terky-07 <terky@stu.scau.edu.cn>
2025-10-20 22:51:27 +0800

    Add files via upload

---
+++ 1.2.1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{float a=2345678900.001;
+ float b=3;
+ cout<<fixed<<a<<endl;
+ float c=a+b;
+ cout<<c<<endl;
+ return 0;
+ } 
---
+++ 1.2.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+double a,b,c;
+cin>>a>>b>>c;
+double sum=a+b+c;
+cout<<sum<<endl;
+	return 0;
+	 
+}
